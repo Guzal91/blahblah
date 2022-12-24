9fb87c48d8e9f01c2561c0905d02ef8ce20488fb
Guzal <gkenisarina91@gmail.com>
2022-12-24 23:14:48 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,27 @@
+var me = prompt('Введите своё имя');
+console.log('Меня зовут Гузаль');
+
+var age = prompt('Введите свой возраст');
+console.log(age);
+
+var problem1 = prompt('Решите пример 6 + 3 = ?');
+console.log('Пример 1: 6 + 3 = 9. Ваш ответ ' + problem1);
+
+var problem2 = prompt('Решите пример 10 - 3 = ?');
+console.log('Пример 2: 10 - 3 = 7. Ваш ответ ' + problem2);
+
+var problem3 = prompt('Решите пример 5 x 5 = ?');
+console.log('Пример 3: 5 x 5 = 25. Ваш ответ ' + problem3);
+
+var problem4 = prompt('Решите пример 100 : 10 = ?');
+console.log('Пример 4: 100 : 10 = 10. Ваш ответ ' + problem4);
+
+var problem5 = prompt('Решите пример 15 % 4 = ?');
+console.log('Пример 5: 15 % 4 = 3. Ваш ответ ' + problem5);
+
+alert('Откройте консоль для просмотра!');
+
+var x = +prompt('Введите первое число');
+var y = +prompt('Введите второе число');
+var z = +prompt('Введите третье число');
+alert( (x + y + z) / 2 );
